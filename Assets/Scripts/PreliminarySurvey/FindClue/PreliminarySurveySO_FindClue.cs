fc87e2caf02cda011b27dee9acea58c592f1693b
VeryMalangCow <127908142+VeryMalangCow@users.noreply.github.com>
2024-06-09 19:22:56 +0900

    Update_BICver_0.2.3_Fix

--- Assets/Scripts/PreliminarySurvey/FindClue/PreliminarySurveySO_FindClue.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PSSO_FC", menuName = "Scriptable Object/PSSO/Find Clue", order = int.MaxValue)]
-[System.Serializable]
-public class PreliminarySurveySO_FindClue : PreliminarySurveySO
-{
-    [SerializeField] public string answerNum;
-    [SerializeField] public GameObject[] clues = new GameObject[8];
-}
